fdebd46dbf2f697c842382e511ad66e538aa276b
gmaggiodev <gmaggio@talentia-software.com>
2020-10-14 17:17:41 +0200

    test

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const { execSync } = require('child_process');
 
-let id = "6d4e7d3";
+let id = "6d4e7d324ada9f4421fc832b5ac012692cb40648";
 let folder = "folder"
 
 // Check the content using git diff-tree
-let changes = execSync(`git diff-tree --no-commit-id --name-only -z -r ${id} ${folder}`);
-console.log("executing: " + `git diff-tree --no-commit-id --name-only -z -r ${id} ${folder}`);
+let changes = execSync(`git diff-tree --no-commit-id --name-only -z -r ${id} -- ${folder}`);
+console.log("executing: " + `git diff-tree --no-commit-id --name-only -z -r ${id} -- ${folder}`);
 console.log(`changes for ${id} - ${changes}`);
